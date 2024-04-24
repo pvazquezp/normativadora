9ba3f4c97f2baaa824c8d84fbcf0a7c171c8ef4f
pvazquezp <151188587+pvazquezp@users.noreply.github.com>
2024-04-24 20:20:36 +0200

    Change Contoso Logo Chat.tsx

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -10,7 +10,7 @@ import { isEmpty } from "lodash";
 import DOMPurify from 'dompurify';
 
 import styles from "./Chat.module.css";
-import Contoso from "../../assets/Contoso.svg";
+import Ntt_Data_Logo from "../../assets/Ntt_Data_Logo.svg";
 import { XSSAllowTags } from "../../constants/xssAllowTags";
 
 import {
@@ -649,7 +649,7 @@ const Chat = () => {
                         {!messages || messages.length < 1 ? (
                             <Stack className={styles.chatEmptyState}>
                                 <img
-                                    src={ui?.chat_logo ? ui.chat_logo : Contoso}
+                                    src={ui?.chat_logo ? ui.chat_logo : Ntt_Data_Logo}
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
